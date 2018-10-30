3ae117491243a3d500b97a3fc1929d2b4e832b91
Bruno Oliveira <brunosol@gmail.com>
2018-10-30 14:00:21 -0300

    Closes #11

--- scripts/170-nfs-mount.sh
+++ scripts/170-nfs-mount.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-######################################################################################################################################################################################################
+################################################################################
 # Autora: Renata Carvalho
 # Data: 28/11/17
-# Versão: 1.0
-# Descrição: Esse script configura o /etc/fstab para fazer a montagem de arquivos da rede.
-######################################################################################################################################################################################################
+# Configura o /etc/fstab para fazer a montagem de arquivos da rede.
+################################################################################
 
 if [ ! -f  "/etc/fstab" ]; then
 	exit 1;
@@ -18,5 +17,5 @@ if ! grep -q "tauari" /etc/fstab; then
 	# Backup do antigo
 	cp /etc/fstab /etc/fstab-old
 
-	echo "tauari:/dados/aplcmc/suporte /mnt/suporte nfs timeo=5,retrans=1,retry=0,rw,bg,intr,_netdev,soft   0   0" >> /etc/fstab
+	echo "tauari:/dados/aplcmc/suporte /mnt/suporte nfs timeo=5,retrans=1,retry=0,ro,bg,intr,_netdev,soft   0   0" >> /etc/fstab
 fi
