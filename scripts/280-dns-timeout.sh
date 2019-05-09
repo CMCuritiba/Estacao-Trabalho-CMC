34228645b23b783a1b44a1dc16e054decc9c0374
João Pedro Galera <suporte.estag2@dif28.cmc.pr.gov.br>
2019-05-09 11:29:18 -0300

    ETv4 migrado para o Mint Mate 19

--- scripts/280-dns-timeout.sh
+++ scripts/280-dns-timeout.sh
@@ -3,6 +3,14 @@
 # Coloca opção no arquivo base do resolv
 # Não testado, deve funcionar
 # O resto deve ser gerado dinamicamente
+if [ ! -d /etc/resolvconf ]; then
+	mkdir /etc/resolvconf
+fi
+
+if [ ! -d /etc/resolvconf/resolv.conf.d ]; then
+	mkdir /etc/resolvconf/resolv.conf.d
+fi
+
 
 echo "options timeout:1 attempts:1 rotate" > /etc/resolvconf/resolv.conf.d/base
 
