58ae3bcbc4b10c45eb60bf61ddcd055e351414bd
João Pedro Galera <suporte.estag2@dif28.cmc.pr.gov.br>
2019-05-10 13:34:58 -0300

    Modificando script no VNC para verificar se arquivo do autostart existe

--- scripts/220-vnc.sh
+++ scripts/220-vnc.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Atualiza o launcher do vino-server para que inicie automaticamente
-if [ -f /etc/xdg/autostart/vino-server.desktop ]; then
-	rm /etc/xdg/autostart/vino-server.desktop
-fi
-
-echo '[Desktop Entry]
+if [ ! -f /etc/xdg/autostart/vino-server.desktop ]; then
+	echo '[Desktop Entry]
 Type=Application
 Terminal=false
 Name=Vino
 Exec=/usr/lib/vino/vino-server --sm-disable
 X-MATE-Autostart-enabled=true' > /etc/xdg/autostart/vino-server.desktop
+else
+	echo 'X-MATE-Autostart-enabled=true' > /etc/xdg/autostart/vino-server.desktop;
+fi
+
 
 # Faz usuario habilitar vino toda vez que ligar
 # Desabilitado pq tem lock
