101730ea281b44e34b9a86558298fcf02a0774d3
brunosoli <brunosol@gmail.com>
2019-12-19 10:21:07 -0300

    Corrige configuração de atualizações fix #52

--- scripts/030-instala-programas.sh
+++ scripts/030-instala-programas.sh
@@ -11,14 +11,9 @@ add-apt-repository -y ppa:mozillateam/ppa
 
 if [ -f "/etc/upstream-release/lsb-release" ]; then
 	source "/etc/upstream-release/lsb-release";
-fi;
-
-if [ -z "$DISTRIB_ID" ]; then
-	DISTRIB_ID=Ubuntu
-fi;
-
-if [ -z "$DISTRIB_RELEASE" ]; then
-	DISTRIB_RELEASE=16.04
+else
+    echo "Versão base do Mint não encontrada"
+    exit 1
 fi;
 
 sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/${DISTRIB_ID}_${DISTRIB_RELEASE}/ /' > /etc/apt/sources.list.d/owncloud-client.list"
@@ -31,15 +26,19 @@ sed -i 's/packages.linuxmint.com/mint-packages.c3sl.ufpr.br/' /etc/apt/sources.l
 # Atualizar os repositórios:
 apt-get update
 
-# Instalar os programas:
-apt-get install -qyf rdesktop audacity owncloud-client owncloud-client-caja vim gthumb
-apt-get install -qyf ncdu sl ttf-mscorefonts-installer gedit
-apt-get install -qyf deadbeef vlc openssh-server
-apt-get install -qyf firefox-esr firefox-esr-locale-pt numlockx
+# Instala programas
+# Acesso remoto
+apt-get install -qyf rdesktop vino openssh-server
+# Midia
+apt-get install -qyf deadbeef vlc audacity exfat-fuse exfat-utils shotwell gthumb
+# Navegacao
+apt-get install -qyf firefox-esr firefox-esr-locale-pt
+# Utilitarios e produtividade
+apt-get install -qyf owncloud-client owncloud-client-caja vim gedit pdfsam unrar ttf-mscorefonts-installer
+# SO
+apt-get install -qyf ncdu numlockx acct
+# Mensageria
 apt-get install -qyf psi psi-translations unity-asset-pool
-apt-get install -qyf vino pdfsam unrar
-
-#apt install -qyf oracle-java8-installer oracle-java8-set-default
 
 # Chrome, pq chrome é especial:
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
