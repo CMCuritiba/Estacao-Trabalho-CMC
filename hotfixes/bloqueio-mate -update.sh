aa475ce8829319debe414662e2ed4e07f6d543e7
suporte <suporte@dtic29.cmc.pr.gov.br>
2020-09-14 09:38:51 -0300

    hotfixes87 att

--- hotfixes/bloqueio-mate -update.sh
+++ hotfixes/bloqueio-mate -update.sh
@@ -8,15 +8,6 @@ setfacl -m g:dtic:rx /usr/bin/ccsm
 setfacl -m g:dtic:rx /usr/bin/mate-network-properties
 
 # Se houver dif no grupo sudoers atualiza para dtic
-	if grep -w "dif" /etc/sudoers; then
+if grep -w "dif" /etc/sudoers; then
 	sed -i 's/dif/dtic/g' /etc/sudoers
-
-# Adiciona dtic ao sudoers caso não exista
-elif ! grep -w "dtic" /etc/sudoers; then
-	sed -i '/%sudo/a%dtic\tALL=(ALL:ALL) ALL' /etc/sudoers
-fi
-
-# Adiciona o suporte ao sudoers caso não exista
-if ! grep -w "suporte" /etc/sudoers; then
-	echo 'suporte   ALL=(ALL:ALL) ALL' >> /etc/sudoers
 fi
