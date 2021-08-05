d005cc2273dd63ecb3e49273fe600428944ea0d5
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2021-08-05 09:33:59 -0300

    Garante acesso ao cups

--- scripts/250-configura-cups.sh
+++ scripts/250-configura-cups.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Garante que a linha não está comentada
+# Garante acesso vindo da TI
+if ! grep -q "10\.0\.1\.0" /etc/cups/cupsd.conf; then
+    sed -i 's/Allow @LOCAL/Allow @LOCAL\n  Allow 10.0.1.0\/24/g' /etc/cups/cupsd.conf
+fi
 
 # Desativa o discovery do cups
 if ! grep -q "^BrowseRemoteProtocols none" /etc/cups/cups-browsed.conf; then
-	echo 'BrowseRemoteProtocols none' >> /etc/cups/cups-browsed.conf;
+    # Garante que a linha não está comentada
+    echo 'BrowseRemoteProtocols none' >>/etc/cups/cups-browsed.conf
 fi
 
 service cups restart
