6037616b8faab99969f74e1a2622d3b9f4df0ae9
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2021-09-30 14:59:09 -0300

    Instala pacote em ETs de produção
    Adicionalmente, explicita argumentos do comando XML.

--- scripts/260-bloqueio-mate.sh
+++ scripts/260-bloqueio-mate.sh
@@ -56,7 +56,7 @@ fi
 udisks_policy="/usr/share/polkit-1/actions/org.freedesktop.UDisks2.policy"
 for fsm in {filesystem-mount,filesystem-unmount-others,filesystem-mount-system}; do
     for defaul_acl in {allow_any,allow_inactive,allow_active}; do
-        xmlstarlet edit -L \
+        xmlstarlet edit --inplace \
             --update "//policyconfig/action[@id='org.freedesktop.udisks2.$fsm']/defaults/$defaul_acl" \
             --value 'yes' \
             "$udisks_policy"
