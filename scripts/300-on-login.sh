8f6972fd6e2fcae51f6941f330461a4db4605e9c
Sandro Izumi <64553030+sandroizumi@users.noreply.github.com>
2021-11-23 09:29:20 -0300

    Update 300-on-login.sh

--- scripts/300-on-login.sh
+++ scripts/300-on-login.sh
@@ -6,6 +6,7 @@ if [ \$(id -u) = 0 ]
 then
     /usr/bin/chattr -f +i /home/*/Desktop/Suporte.desktop
     /usr/bin/chattr -f +i /home/*/Desktop/Firefox.desktop
+    /usr/bin/chattr -f +i /home/*/Desktop/google-chrome.desktop
     /usr/bin/chattr -f +i /home/*/Desktop/Rainbow.desktop
 fi" > /usr/local/cmc/scripts/on-login.sh
 
