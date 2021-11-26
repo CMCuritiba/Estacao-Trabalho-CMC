2828782af799f74a7907ed2d6da375128d7b1762
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2021-11-26 14:15:53 -0300

    Restringe acesso SSH

--- scripts/210-ssh-server.sh
+++ scripts/210-ssh-server.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-# Habilita login como root
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+# Desabilita login SSH para root:
+SSHD="/etc/ssh/sshd_config"
+logger "Desabilitando login SSH para root"
+if grep -q "^PermitRootLogin yes" "$SSHD"; then
+    sed -i '/^PermitRootLogin/c\#PermitRootLogin no' "$SSHD"
+fi
+
+# Restringe SSH apenas para usuários da DTIC:
+logger "Restringindo SSH apenas para usuários da DTIC"
+if grep -q "^AllowGroups" "$SSHD"; then
+    # replace
+    sed -i '/^AllowGroups/c\AllowGroups dtic' "$SSHD"
+else
+    # add
+    echo "AllowGroups dtic" >>"$SSHD"
+fi
 
 # Reinicia serviço para aplicar
 service ssh restart
