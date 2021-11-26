3b66b3e3ca0032b8a66fd9e77b6c85b9cc149177
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2021-11-26 16:23:19 -0300

    Parametriza e melhora alterações no cups

--- scripts/250-configura-cups.sh
+++ scripts/250-configura-cups.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+CUPSD="/etc/cups/cupsd.conf"
+CUPS_BROWSED="/etc/cups/cups-browsed.conf"
+
 # Garante acesso vindo da TI
-if ! grep -q "10\.0\.1\.0" /etc/cups/cupsd.conf; then
-    sed -i 's/Allow @LOCAL/Allow @LOCAL\n  Allow 10.0.1.0\/24/g' /etc/cups/cupsd.conf
+if ! grep -q "$DTIC_NETWORK" "$CUPSD"; then
+    # sed -i "/^<Location.*/a \ \ Allow @LOCAL\n  Allow $DTIC_NETWORK" "$CUPSD"
+    sed -i "s|Allow @LOCAL|Allow @LOCAL\n  Allow $DTIC_NETWORK|g" "$CUPSD"
 fi
 
 # Desativa o discovery do cups
-if ! grep -q "^BrowseRemoteProtocols none" /etc/cups/cups-browsed.conf; then
-    # Garante que a linha não está comentada
-    echo 'BrowseRemoteProtocols none' >>/etc/cups/cups-browsed.conf
+if ! grep -q "^BrowseRemoteProtocols none" "$CUPS_BROWSED"; then
+    if grep -q "^BrowseRemoteProtocols" "$CUPS_BROWSED"; then
+        sed -i '/^BrowseRemoteProtocols/c\BrowseRemoteProtocols none' "$CUPS_BROWSED"
+    else
+        # Garante que a linha não está comentada
+        echo 'BrowseRemoteProtocols none' >>"$CUPS_BROWSED"
+    fi
 fi
 
 service cups restart
