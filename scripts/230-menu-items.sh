0d9b7599c0c1b714171a7139d77ec3cca2e8cfeb
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2021-12-08 16:43:03 -0300

    Remove aplicações obsoletas
    Aplicações não existem mais no mint

--- scripts/230-menu-items.sh
+++ scripts/230-menu-items.sh
@@ -4,24 +4,13 @@
 # Não importa se esse script falhar, pode prosseguir
 set +e
 
-mv /usr/share/applications/gnome-user-guide.desktop /usr/share/applications/gnome-user-guide.desktop.disable
 mv /usr/share/applications/blueberry.desktop /usr/share/applications/blueberry.desktop.disable
 mv /usr/share/applications/bluetooth-sendto.desktop /usr/share/applications/bluetooth-sendto.desktop.disable
 mv /usr/share/applications/nm-connection-editor.desktop /usr/share/applications/nm-connection-editor.desktop.disable
 mv /usr/share/applications/mintupdate.desktop /usr/share/applications/mintupdate.desktop.disable
 mv /usr/share/applications/mintreport.desktop /usr/share/applications/mintreport.desktop.disable
 mv /usr/share/applications/lightdm-settings.desktop /usr/share/applications/lightdm-settings.desktop.disable
-#mv /usr/share/applications/menulibre.desktop /usr/share/applications/menulibre.desktop.disable
-#mv /usr/share/applications/JB-controlpanel-jdk8.desktop /usr/share/applications/JB-controlpanel-jdk8.desktop.disable
-#mv /usr/share/applications/JB-java-jdk8.desktop /usr/share/applications/JB-java-jdk8.desktop.disable
-#mv /usr/share/applications/JB-mission-control-jdk8.desktop /usr/share/applications/JB-mission-control-jdk8.desktop.disable
-#mv /usr/share/applications/JB-javaws-jdk8.desktop /usr/share/applications/JB-javaws-jdk8.desktop.disable
-#mv /usr/share/applications/JB-jconsole-jdk8.desktop /usr/share/applications/JB-jconsole-jdk8.desktop.disable
-#mv /usr/share/applications/JB-policytool-jdk8.desktop /usr/share/applications/JB-policytool-jdk8.desktop.disable
-#mv /usr/share/applications/JB-jvisualvm-jdk8.desktop /usr/share/applications/JB-jvisualvm-jdk8.desktop.disable
-mv /usr/share/applications/gnome-network-properties.desktop.disable /usr/share/applications/gnome-network-properties.desktop
 mv /usr/share/applications/mintwelcome.desktop /usr/share/applications/mintwelcome.desktop.disable
-mv /usr/share/applications/users.desktop /usr/share/applications/users.desktop.disable
 
 # Não importa se um dos mvs falhar
 exit 0
