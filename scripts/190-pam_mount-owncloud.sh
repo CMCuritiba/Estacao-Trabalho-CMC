4e7aa3e213f88185f8486260925f321aae075c38
fabiokleis <fabiohkrc@gmail.com>
2022-02-02 14:49:01 -0300

    removido o mkdir -p /home/urameshi/Nuvem do /etc/profile

--- scripts/190-pam_mount-owncloud.sh
+++ scripts/190-pam_mount-owncloud.sh
@@ -2,8 +2,6 @@
 
 DEBIAN_FRONTEND=nointeractive apt install davfs2  libpam-mount -yq
 
-echo "mkdir -p $HOME/Nuvem" >> /etc/profile
-
 if ! cmp -s /etc/security/pam_mount.conf.xml ../arquivos/pam_mount.template.conf; then
     cp -f ../arquivos/pam_mount.template.conf /etc/security/pam_mount.conf.xml
 fi
