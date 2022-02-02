4e0ad869c5b5c6ceb10bf1d9f7902088a4f8972c
fabiokleis <fabiohkrc@gmail.com>
2022-02-02 08:44:27 -0300

    agora o diretório Nuvem é criado diretamenta com /etc/profile

--- scripts/190-pam_mount-owncloud.sh
+++ scripts/190-pam_mount-owncloud.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 DEBIAN_FRONTEND=nointeractive apt install davfs2  libpam-mount -yq
-chmod u+s /sbin/mount.davfs
 
-mkdir -p "$HOME/owncloud"
-chown "$USER":"$USER" "$HOME/owncloud"
+echo "mkdir -p $HOME/Nuvem" >> /etc/profile
 
 if ! cmp -s /etc/security/pam_mount.conf.xml ../arquivos/pam_mount.template.conf; then
     cp -f ../arquivos/pam_mount.template.conf /etc/security/pam_mount.conf.xml
