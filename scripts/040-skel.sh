8edfe57744ed670480b54e010a1dc5c37e6af4eb
Bruno Oliveira <brunosoli@users.noreply.github.com>
2022-04-13 15:54:39 -0300

    chore: corrige typo

--- scripts/040-skel.sh
+++ scripts/040-skel.sh
@@ -145,7 +145,7 @@ echo '# GIMP sessionrc
 # Política de privacidade
 echo '#!/bin/bash
 
-zenity --text-info --title="Política de informatica" --checkbox="Eu li e aceito as condições estipuladas a cima" --filename="/usr/local/cmc/politica-informatica.txt" --height=600 --width=800 2>/dev/null
+zenity --text-info --title="Política de informatica" --checkbox="Eu li e aceito as condições estipuladas acima" --filename="/usr/local/cmc/politica-informatica.txt" --height=600 --width=800 2>/dev/null
 if [ $? != 0 ]; then
         touch "$HOME/.forcelogout"
 else
