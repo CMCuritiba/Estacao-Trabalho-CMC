f8cbaf2b4e21a6d8816b92a3767122624b6e2bba
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2024-02-19 16:48:25 -0300

    fix(uc): erro ao bater versões

--- arquivos/zoom-update.sh
+++ arquivos/zoom-update.sh
@@ -4,18 +4,18 @@ instalador_remoto="/mnt/suporte/etv4/scripts/files/zoom_amd64.deb"
 instalador_local="/tmp/zoom_amd64.deb"
 
 if [ -f "$instalador_remoto" ]; then
-    ZOOM_VERSION_AVAILABLE=$(dpkg-deb -I $instalador_remoto | grep Version)
+    ZOOM_VERSION_AVAILABLE=$(dpkg-deb -I $instalador_remoto | grep Version | awk '{print $2}')
     # echo "ultima versão disponível para download: $ZOOM_VERSION_AVAILABLE"
 else
     if ! wget --quiet --continue "https://zoom.us/client/latest/zoom_amd64.deb" --output-document="$instalador_local"; then
         logger "[$0] ERRO ao baixar instalador zoom da internet."
         exit 1
     fi
-    ZOOM_VERSION_AVAILABLE=$(dpkg-deb -I "$instalador_local" | grep Version)
+    ZOOM_VERSION_AVAILABLE=$(dpkg-deb -I "$instalador_local" | grep Version | awk '{print $2}')
     # echo "ultima versão disponível para download: $ZOOM_VERSION_AVAILABLE"
 fi
 
-ZOOM_VERSION_INSTALLED=$(apt-cache policy zoom | grep "Installed:" | sed -e 's/.*Installed: \(.*\)/\1/')
+ZOOM_VERSION_INSTALLED=$(apt-cache policy zoom | grep "Installed:" | awk '{print $2}')
 # echo "versão do zoom instalada: $ZOOM_VERSION_INSTALLED"
 if [ -f "$instalador_remoto" ] && [[ "$ZOOM_VERSION_INSTALLED" != *"$ZOOM_VERSION_AVAILABLE"* ]]; then
     # echo "pasta mnt/suporte encontrada"
