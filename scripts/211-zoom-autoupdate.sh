085d1d070301dcb5b633228f4b0bf37842be3945
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2024-02-19 16:30:53 -0300

    feat(uc): atualiza zoom diariamente
    Adiciona timer para o serviço de atualização do Zoom.
    @lucasdeabreufonseca
    
    re #141

--- scripts/211-zoom-autoupdate.sh
+++ scripts/211-zoom-autoupdate.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
-#verificar se usuário está como root
+# Verifica se usuário está como root
 if [[ $UID != 0 ]]; then
-  echo "Executar este script como root."
-  exit 1
+    echo "Executar este script como root."
+    exit 1
 fi
 
-#copia arquivo zoom-updater para seu respectivo diretorio
+# Copia arquivo zoom-updater para seu respectivo diretorio
 if [ -f "../arquivos/zoom-update.sh" ]; then
-   cp "../arquivos/zoom-autoupdate.sh" /usr/local/cmc/zoom-updater.sh
-   chmod +x /usr/local/cmc/zoom-updater.sh
+    cp "../arquivos/zoom-autoupdate.sh" /usr/local/cmc/zoom-updater.sh
+    chmod +x /usr/local/cmc/zoom-updater.sh
 else
-   echo arquivo zoom updater não encontrado
-   exit 1
+    echo "Arquivo zoom updater não encontrado"
+    exit 1
 fi
 
-#copia arquivo de serviço para seu respectivo diretorio
+# Instala serviço no systemd
 if [ -f "../arquivos/zoom-update.service" ]; then
-   cp ../arquivos/zoom-update.service /etc/systemd/system/zoom-update.service
+    cp ../arquivos/zoom-update.service /etc/systemd/system/zoom-update.service
 else
-   echo arquivo zoom-update.service não encontrado
-   exit 1
+    echo "Arquivo zoom-update.service não encontrado"
+    exit 1
 fi
 
-systemctl enable --now zoom-update.service
+# Cria timer de serviço no systemd
+if [ -f "../arquivos/zoom-update.timer" ]; then
+    cp ../arquivos/zoom-update.timer /etc/systemd/system/zoom-update.timer
+else
+    echo "Arquivo zoom-update.timer não encontrado"
+    exit 1
+fi
+
+systemctl daemon-reload
+systemctl enable zoom-update.service
+systemctl enable zoom-update.timer
 # execute zoom update immediately
 systemctl start zoom-update.service
 # output systemd status/logs
-systemctl --no-pager status zoom-update.service
+# systemctl --no-pager status zoom-update.service
+# systemctl list-timers --all
