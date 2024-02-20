8f77438a5e8075cb525226866c530bbe9844ffce
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2024-02-20 08:53:10 -0300

    fix(uc): evita apt desnecessário
    Adicionalmente, melhora nome de variáveis.

--- arquivos/zoom-update.sh
+++ arquivos/zoom-update.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-instalador_remoto="/mnt/suporte/etv4/scripts/files/zoom_amd64.deb"
-instalador_local="/tmp/zoom_amd64.deb"
+instalador_rede="/mnt/suporte/etv4/scripts/files/zoom_amd64.deb"
+instalador_internet="/tmp/zoom_amd64.deb"
 
-if [ -f "$instalador_remoto" ]; then
-    ZOOM_VERSION_AVAILABLE=$(dpkg-deb -I $instalador_remoto | grep Version | awk '{print $2}')
+if [ -f "$instalador_rede" ]; then
+    ZOOM_VERSION_AVAILABLE=$(dpkg-deb -I $instalador_rede | grep Version | awk '{print $2}')
     # echo "ultima versão disponível para download: $ZOOM_VERSION_AVAILABLE"
 else
-    if ! wget --quiet --continue "https://zoom.us/client/latest/zoom_amd64.deb" --output-document="$instalador_local"; then
+    if ! wget --quiet --continue "https://zoom.us/client/latest/zoom_amd64.deb" --output-document="$instalador_internet"; then
         logger "[$0] ERRO ao baixar instalador zoom da internet."
         exit 1
     fi
-    ZOOM_VERSION_AVAILABLE=$(dpkg-deb -I "$instalador_local" | grep Version | awk '{print $2}')
+    ZOOM_VERSION_AVAILABLE=$(dpkg-deb -I "$instalador_internet" | grep Version | awk '{print $2}')
     # echo "ultima versão disponível para download: $ZOOM_VERSION_AVAILABLE"
 fi
 
 ZOOM_VERSION_INSTALLED=$(apt-cache policy zoom | grep "Installed:" | awk '{print $2}')
 # echo "versão do zoom instalada: $ZOOM_VERSION_INSTALLED"
-if [ -f "$instalador_remoto" ] && [[ "$ZOOM_VERSION_INSTALLED" != *"$ZOOM_VERSION_AVAILABLE"* ]]; then
-    # echo "pasta mnt/suporte encontrada"
-    export DEBIAN_FRONTEND=noninteractive
-    if ! apt-get install -qy "$instalador_remoto"; then
-        logger "[$0] ERRO ao instalar zoom via rede local."
-    fi
-else
-    # echo pasta mnt/suporte não encontrada
-    export DEBIAN_FRONTEND=noninteractive
-    if ! apt-get install -qy "$instalador_local"; then
-        logger "[$0] ERRO ao instalar zoom da internet."
+if [[ "$ZOOM_VERSION_INSTALLED" != *"$ZOOM_VERSION_AVAILABLE"* ]]; then
+    if [ -f "$instalador_rede" ]; then
+        # echo "pasta mnt/suporte encontrada"
+        export DEBIAN_FRONTEND=noninteractive
+        if ! apt-get install -qy "$instalador_rede"; then
+            logger "[$0] ERRO ao instalar zoom via rede local."
+        fi
+    else
+        # echo pasta mnt/suporte não encontrada
+        export DEBIAN_FRONTEND=noninteractive
+        if ! apt-get install -qy "$instalador_internet"; then
+            logger "[$0] ERRO ao instalar zoom da internet."
+        fi
     fi
 fi
