e1a031d6035c8b70d3bd5d13e66ff3d84de5c946
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2024-02-22 11:03:20 -0300

    chore(browser): atualiza favoritos e navegador padrão

--- scripts/060-configura-browsers.sh
+++ scripts/060-configura-browsers.sh
@@ -9,51 +9,48 @@ function buildBookmarksFirefox() {
         \"toplevel_name\": \"Favoritos Gerenciados da CMC\"
       },"
 
-    while [ $# -gt 0 ]; do
-        x=${1}
+    for i in "${!BOOKMARKS[@]}"; do
+        echo -n "{\"url\": \"${BOOKMARKS[$i]}\",\"name\": \"$i\"},"
+    done
 
-        echo -n "
-      {
-        \"url\": \"${BOOKMARKS[$x]}\",
-        \"name\": \"$x\"
-      }"
-
-        [ $# -gt 1 ] && echo -n ','
-        shift
+    echo -n "{\"name\": \"SGP\",\"children\":["
+    last="${SGP[@]: -1}"
+    for i in "${!SGP[@]}"; do
+        echo -n "{\"url\": \"${SGP[$i]}\",\"name\": \"$i\"}"
+        if [ "${SGP[$i]}" != "$last" ]; then
+            echo -n ","
+        fi
     done
 
     # Fecha JSON
-    echo -n "
-    ]
-  }
-}"
+    echo -n "]}]}}"
 }
 
 function buildBookmarksChrome() {
-    # shellcheck disable=SC2016
     echo -n "{
   \"DownloadDirectory\": \"/home/\${user_name}/Downloads\",
   \"DefaultBrowserSettingEnabled\": false,
   \"DisablePrintPreview\": true,
-  \"ManagedBookmarks\": ["
-
-    while [ $# -gt 0 ]; do
-        x=${1}
-
-        echo -n "
+  \"ManagedBookmarks\": [
     {
-      \"url\": \"${BOOKMARKS[$x]}\",
-      \"name\": \"$x\"
-    }"
+      \"toplevel_name\": \"Favoritos Gerenciados da CMC\"
+    },"
 
-        [ $# -gt 1 ] && echo -n ','
-        shift
+    for i in "${!BOOKMARKS[@]}"; do
+        echo -n "{\"url\": \"${BOOKMARKS[$i]}\",\"name\": \"$i\"},"
+    done
+
+    echo -n "{\"name\": \"SGP\",\"children\":["
+    last="${SGP[@]: -1}"
+    for i in "${!SGP[@]}"; do
+        echo -n "{\"url\": \"${SGP[$i]}\",\"name\": \"$i\"}"
+        if [ "${SGP[$i]}" != "$last" ]; then
+            echo -n ","
+        fi
     done
 
     # Fecha JSON
-    echo -n "
-  ]
-}"
+    echo -n "]}]}"
 }
 
 # FIREFOX
@@ -82,34 +79,43 @@ EnableProfileMigrator=false" >/usr/lib/firefox/browser/override.ini
 # Lista de favoritos gerenciados pela DTIC
 declare -A BOOKMARKS
 BOOKMARKS["Câmara Municipal de Curitiba"]="https://www.cmc.pr.gov.br/"
+BOOKMARKS["Intranet"]="https://intranet.cmc.pr.gov.br/"
 BOOKMARKS["Correio"]="https://correio.cmc.pr.gov.br/"
 BOOKMARKS["SPL II"]="https://www.cmc.pr.gov.br/spl/"
 BOOKMARKS["SPAe"]="https://spae.cmc.pr.gov.br/"
 BOOKMARKS["Nuvem"]="https://nuvem.cmc.pr.gov.br/"
+BOOKMARKS["RH-Online"]="https://www.cmc.pr.gov.br/portalrh/"
 BOOKMARKS["Eloweb Gestão Pública"]="https://acesso.cmcuritiba.eloweb.net/"
 BOOKMARKS["Chamados"]="https://chamados.cmc.pr.gov.br/"
 BOOKMARKS["Suporte"]="https://chamados.cmc.pr.gov.br/#knowledge_base"
 BOOKMARKS["Senha"]="https://senha.cmc.pr.gov.br/"
-BOOKMARKS["APL"]="https://intranet.cmc.pr.gov.br/apl/"
-BOOKMARKS["SPA - Legado"]="https://www.cmc.pr.gov.br/spa/"
+BOOKMARKS["APL"]="https://apl.cmc.pr.gov.br/"
+BOOKMARKS["BIB"]="https://www.cmc.pr.gov.br/bib/"
+BOOKMARKS["SPA - Legado"]="https://spa.cmc.pr.gov.br/"
+BOOKMARKS["Cerimonial"]="https://cerimonial.cmc.pr.gov.br/"
 BOOKMARKS["Minha Biblioteca"]="https://minha-biblioteca.cmc.pr.gov.br/"
 BOOKMARKS["Registro de frequência"]="https://www.cmc.pr.gov.br/registro-frequencia/"
 BOOKMARKS["Zoom"]="https://cmc-pr-gov-br.zoom.us/"
+BOOKMARKS["Rainbow"]="https://web.openrainbow.com/"
+BOOKMARKS["GIP"]="https://gip.cmc.pr.gov.br/"
+BOOKMARKS["Legisladoc"]="https://legisladoc.curitiba.pr.gov.br/"
 BOOKMARKS["Prefeitura Municipal de Curitiba"]="https://www.curitiba.pr.gov.br/"
-BOOKMARKS["Elotech"]="https://elotech.movidesk.com/Account/Login?ReturnUrl=%2f"
+
+declare -A SGP
+SGP["SGP WEB"]="https://sgpweb.curitiba.pr.gov.br"
+SGP["Consolidação"]="http://172.27.115.72:7070/Consolidacao/pages/login.jsf"
+SGP["Portal Contratos"]="http://portalcontratos.curitiba.pr.gov.br:7070/PortalContratos/portal.do?formAction=init"
+SGP["Solicitações Web"]="http://sgp-web.curitiba.pr.gov.br/sgp/seg/login.do"
+SGP["Consulta Licitações"]="http://consultalicitacao.curitiba.pr.gov.br:9090/ConsultaLicitacoes/"
 
 # Cria e configura o arquivo policies.json na pasta /usr/lib/firefox/distribution/
 # Referencia para policies:
 # https://github.com/mozilla/policy-templates/blob/master/README.md#bookmarks
 # https://github.com/mozilla/policy-templates/blob/v2.11/README.md
 favsFirefox="/usr/lib/firefox/distribution/policies.json" # type: json file
-buildBookmarksFirefox "${!BOOKMARKS[@]}" | jq >"$favsFirefox"
-
+buildBookmarksFirefox "${!BOOKMARKS[@]}" | jq . >"$favsFirefox"
 
 # GOOGLE CHROME
-
-# Coloca o Google Chrome como navegador padrão
-update-alternatives --install /usr/bin/x-www-browser x-www-browser /usr/bin/google-chrome-stable 999
 
 # Cria e configura o arquivo cmc.json na pasta /etc/opt/chrome/policies/managed/ e recommended/
 # Referencia para policies:
@@ -118,11 +124,11 @@ mkdir -p /etc/opt/chrome/policies/recommended
 mkdir -p /etc/opt/chrome/policies/managed
 
 favsChrome="/etc/opt/chrome/policies/managed/cmc.json" # type: json file
-buildBookmarksChrome "${!BOOKMARKS[@]}" | jq >"$favsChrome"
+buildBookmarksChrome "${!BOOKMARKS[@]}" | jq . >"$favsChrome"
 
 echo '{
   "HomepageLocation": "https://www.cmc.pr.gov.br/",
   "RestoreOnStartup": 4,
   "RestoreOnStartupURLs": ["https://www.cmc.pr.gov.br/"],
   "HomepageIsNewTabPage": false
-}' | jq >/etc/opt/chrome/policies/recommended/cmc.json
+}' | jq . >/etc/opt/chrome/policies/recommended/cmc.json
