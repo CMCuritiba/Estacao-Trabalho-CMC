9d824320d958dbf295c67f16a8aaf8ad690b6bf9
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2024-02-22 12:06:01 -0300

    chore: atualiza modificados

--- scripts/999-arquivos-modificados.sh
+++ scripts/999-arquivos-modificados.sh
@@ -11,11 +11,14 @@ ln -sf /root/.bashrc /usr/local/cmc/modificados/bashrc
 ln -sf /root/.bash_aliases /usr/local/cmc/modificados/bash_aliases
 
 # Configs de browsers
-ln -sf /usr/lib/firefox/defaults/pref/local-settings.js /usr/local/cmc/modificados/local-settings.js
-ln -sf /usr/lib/firefox/mozilla.cfg /usr/local/cmc/modificados/mozilla.cfg
-ln -sf /usr/lib/firefox/browser/override.ini /usr/local/cmc/modificados/override.ini
+ln -sf /usr/lib/firefox/defaults/pref/local-settings.js \
+    /usr/local/cmc/modificados/firefox-local-settings.js
+ln -sf /usr/lib/firefox/mozilla.cfg \
+    /usr/local/cmc/modificados/mozilla.cfg
+ln -sf /usr/lib/firefox/browser/override.ini \
+    /usr/local/cmc/modificados/firefox-override.ini
 ln -sf /usr/lib/firefox/distribution/policies.json \
-    /usr/local/cmc/modificados/policies.json
+    /usr/local/cmc/modificados/firefox-policies.json
 ln -sf /etc/opt/chrome/policies/managed/cmc.json \
     /usr/local/cmc/modificados/chrome-policies-managed-cmc.json
 ln -sf /etc/opt/chrome/policies/recommended/cmc.json \
@@ -28,14 +31,13 @@ ln -sf /etc/xdg/autostart/forcelogout.desktop /usr/local/cmc/modificados/forcelo
 ln -sf /usr/bin/gnome-terminal /usr/local/cmc/modificados/gnome-terminal
 ln -sf /usr/bin/mintupdate /usr/local/cmc/modificados/mintupdate
 ln -sf /usr/bin/mintreport /usr/local/cmc/modificados/mintreport
-#ln -sf /usr/bin/menulibre /usr/local/cmc/modificados/menulibre
 ln -sf /usr/bin/cinnamon-desktop-editor /usr/local/cmc/modificados/cinnamon-desktop-editor
 ln -sf /usr/bin/nm-connection-editor /usr/local/cmc/modificados/nm-connection-editor
 ln -sf /usr/bin/nm-applet /usr/local/cmc/modificados/nm-applet
 
 # dconf
-ln -sf /etc/dconf/profile/user /usr/local/cmc/modificados/user
-ln -sf /etc/dconf/db/local.d/01-cmc /usr/local/cmc/modificados/01-cmc
+ln -sf /etc/dconf/profile/user /usr/local/cmc/modificados/dconf-user
+ln -sf /etc/dconf/db/local.d/01-cmc /usr/local/cmc/modificados/dconf-01-cmc
 
 # AD e PAM
 ln -sf /etc/pam.d/common-session /usr/local/cmc/modificados/common-session
@@ -60,7 +62,7 @@ ln -sf /etc/cups/cups-browsed.conf /usr/local/cmc/modificados/cups-browsed.conf
 ln -sf /etc/nsswitch.conf /usr/local/cmc/modificados/nsswitch.conf
 
 # Sudoers
-ln -sf /etc/sudoers /usr/local/cmc/modificados/sudoers
+ln -sf /etc/sudoers.d/cmc /usr/local/cmc/modificados/sudoers.d-cmc
 
 # Desabilita TTYs
 ln -sf /etc/X11/xorg.conf /usr/local/cmc/modificados/xorg.conf
@@ -77,3 +79,6 @@ ln -sf /mnt/suporte/etv4/scripts/boot.sh /usr/local/cmc/modificados/boot.sh
 # Assegura permissão de montagem de dispositivos externos (pendrives, HDs, etc)
 ln -sf /usr/share/polkit-1/actions/org.freedesktop.UDisks2.policy \
     /usr/local/cmc/modificados/org.freedesktop.UDisks2.policy
+
+ln -sf /etc/systemd/system/cmc-boot.service \
+    /usr/local/cmc/modificados/cmc-boot.service
