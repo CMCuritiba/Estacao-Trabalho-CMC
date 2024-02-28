a1d3be29e3a48ab0113d4c79aa090f226db59d46
ewerton-dif <ewerton@cmc.pr.gov.br>
2024-02-28 15:40:07 -0300

    fix: ajustes de hostname

--- scripts/199-converte-ad.sh
+++ scripts/199-converte-ad.sh
@@ -2,7 +2,6 @@
 
 # Update e Upgrade inicial
 apt-get update
-apt-get -y upgrade
 
 # Remove configuracao do LDAP
 DEBIAN_FRONTEND=noninteractive apt-get -qyf purge libnss-ldap libpam-ldap nscd nslcd
@@ -10,10 +9,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -qyf purge libnss-ldap libpam-ldap nscd n
 # Instala SSSD, REALM, KERBEROS e ADCLI
 DEBIAN_FRONTEND=noninteractive apt-get -qyf install sssd realmd krb5-user adcli
 
-# Remove resolv.conf para usar configuracao local e apontar para o IP do DNS do AD
-rm -rf /etc/resolv.conf
-echo "nameserver $AD_ADDRESS" > /etc/resolv.conf
-
+# configuracao local e apontar para o IP do DNS do AD
 if ! grep -w "${AD_DOMAIN,,}" "/etc/systemd/resolved.conf"; then
     sed -i "/Domains=cmc.pr.gov.br/c\Domains=${AD_DOMAIN,,}" "/etc/systemd/resolved.conf"
 fi
@@ -35,13 +31,6 @@ echo $HOST > /etc/hostname
 
 # Altera arquivo 'hosts' para o novo nome com o dominio (minusculo)
 sed -i "/^127.0.1.1/c\127.0.1.1\t"$HOST.${AD_DOMAIN,,} "/etc/hosts"
-sed -i "/^127.0.2.1/c\127.0.2.1\t"$HOST.${AD_DOMAIN,,} "/etc/hosts"
-
-# Ajuste o servico SSH para permitir autenticacao de senha
-sed -i "/#PasswordAuthentication yes/c\PasswordAuthentication yes" "/etc/ssh/sshd_config"
-
-# Restarta o servico SSH
-systemctl restart sshd
 
 # Configura arquivo do Kerberos com o nome do dominio e corrige problema de DNS reverso
 echo "[libdefaults]
@@ -63,19 +52,19 @@ echo $AD_JOIN_PWD | realm join -U $AD_JOIN $AD_DOMAIN
 
 # Configuracao do SSSD para apontar para o dominio e manter cache infinito
 echo "[sssd]
-domains = ${AD_DOMAIN,,}
-config_file_version = 2
-services = nss,pam
+domains = ${AD_DOMAIN,,} #
+config_file_version = 2 #
+services = nss,pam #
 
-[nss]
+[nss] #
 
-[pam]
-offline_credentials_expiration = 0
-offline_failed_login_attempts = 0
-offline_failed_login_delay = 0
+[pam] #
+offline_credentials_expiration = 0 #
+offline_failed_login_attempts = 0 #
+offline_failed_login_delay = 0 #
 
 [domain/${AD_DOMAIN,,}]
-realmd_tags = manages-system joined-with-adcli
+realmd_tags = manages-system joined-with-adcli #
 ad_domain = ${AD_DOMAIN,,}
 krb5_realm = $AD_DOMAIN
 
@@ -98,10 +87,10 @@ ldap_user_name = uid
 ldap_user_gid_number = gidNumber
 ldap_user_uid_number = uidNumber
 ldap_user_gecos = displayName
-" > /etc/sssd/sssd.conf
+" >/etc/sssd/conf.d/01-cmc.conf
 
 # Altera permissao do arquivo sssd.conf
-chmod 600 /etc/sssd/sssd.conf
+chmod 600 /etc/sssd/conf.d/01-cmc.conf
 
 # Restarta o servico SSSD
 systemctl restart sssd
@@ -113,7 +102,7 @@ else
     echo -e "\n$DTIC_GID\tALL=(ALL:ALL) ALL" >> /etc/sudoers
 fi
 
-# para funcionar o 'cp' tem que retirar o alias
+# para funcionar o 'cp' sem confirmacao, tem que retirar o alias
 unalias cp
 # Configuracao do nsswitch
 if ! cmp -s /etc/nsswitch.conf ../arquivos/nsswitch.conf.template; then
