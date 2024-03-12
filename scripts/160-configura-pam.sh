142f50cd0f14f192502ee45a9c1bd896bb6dc476
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2024-03-12 12:16:28 -0300

    refactor(pam): simplifica configuração
    Evita sobrescrever alterações padrão.
    
    re #140

--- scripts/160-configura-pam.sh
+++ scripts/160-configura-pam.sh
@@ -6,15 +6,7 @@ if ! cmp -s /etc/nsswitch.conf ../arquivos/nsswitch.conf.template; then
 fi
 
 # Automatiza a criação do diretório HOME após o login
+# Por default, utiliza:
+#   umask=0022
+#   skel=/etc/skel
 pam-auth-update --force --enable mkhomedir
-
-# Configuração do PAM para trabalhar com SSSD
-if ! cmp -s /etc/pam.d/common-auth ../arquivos/common-auth.template; then
-    cp -f --backup=t ../arquivos/common-auth.template /etc/pam.d/common-auth
-fi
-if ! cmp -s /etc/pam.d/common-account ../arquivos/common-account.template; then
-    cp -f --backup=t ../arquivos/common-account.template /etc/pam.d/common-account
-fi
-if ! cmp -s /etc/pam.d/common-session ../arquivos/common-session.template; then
-    cp -f --backup=t ../arquivos/common-session.template /etc/pam.d/common-session
-fi
