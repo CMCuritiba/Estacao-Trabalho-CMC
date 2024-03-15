aa82c76e6f9d33791289e0a81de9b2bb89609b19
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2024-03-15 08:32:25 -0300

    fix(ad): configuração sssd e krb5
    re #140

--- scripts/150-integra-ad.sh
+++ scripts/150-integra-ad.sh
@@ -1,42 +1,33 @@
 #!/bin/bash
 
 # Instala SSSD, REALM, KERBEROS e ADCLI
-DEBIAN_FRONTEND=noninteractive apt-get -qyf install sssd sssd-tools realmd krb5-user samba-common packagekit adcli
+apt-get -qyf install sssd sssd-tools realmd krb5-user samba-common packagekit adcli
 
-# Configura arquivo do Kerberos com o nome do domínio e corrige problema de DNS reverso
-echo "[libdefaults]
-default_realm = ${AD_DOMAIN^^}
-dns_lookup_realm = false
-dns_lookup_kdc = false
-rdns=false
-kdc_timesync = 1
-ccache_type = 4
-forwardable = true
-proxiable = true
-fcc-mit-ticketflags = true
-udp_preference_limit = 0
+if ! grep -iq "$AD_DOMAIN" <<<"$(hostname -f)"; then
+    echo "Hostname não configurado, abortando."
+    exit 1
+fi
 
-[realms]
-    ${AD_DOMAIN^^} = {
-    default_domain = ${AD_DOMAIN,,}
-    kdc = ${AD_DOMAIN,,}
-    admin_server = ${AD_DOMAIN,,}
-}
-
-[domain_realm]
-    .${AD_DOMAIN,,} = ${AD_DOMAIN^^}
-    ${AD_DOMAIN,,} = ${AD_DOMAIN^^}
-" >/etc/krb5.conf
+KRBCONF="/etc/krb5.conf"
+# Configura arquivo do Kerberos com o domínio e evita problema de DNS reverso
+sed -i "s/default_realm.*/default_realm = $AD_DOMAIN/" "$KRBCONF"
+if ! grep -Eiq "\s*rdns.+false$" "$KRBCONF"; then
+    sed -i "/default_realm/a \\\\trdns = false" "$KRBCONF"
+fi
 
 if ! realm list | grep -iq "$AD_DOMAIN"; then
-    # Cria o ticket do Kerberos para encontrar o domínio
-    echo "$AD_JOIN_PASS" | kinit "$AD_JOIN_USER@${AD_DOMAIN^^}"
-
     # Adiciona o computador ao domínio
     echo "$AD_JOIN_PASS" | realm join -U "$AD_JOIN_USER" "${AD_DOMAIN^^}"
 fi
 
-# Configuração do SSSD para apontar para o domínio e manter cache infinito
+# Configuração do SSSD:
+# - mapear IDs linux
+# - configura diretório home
+# - permite login apenas com uid
+# - faz cache de senha (permite login offline) TODO: verificar duração
+# - TODO: filtra usuários
+#
+# Ref: https://docs.aws.amazon.com/directoryservice/latest/admin-guide/join_linux_instance.html
 echo "# SSSD CMC
 
 [sssd]
@@ -44,65 +35,27 @@ domains = ${AD_DOMAIN,,}
 config_file_version = 2
 services = nss, pam
 
-[nss]
-
-[pam]
-# Para não expirar o cache de login
-offline_credentials_expiration = 0
-offline_failed_login_attempts = 0
-offline_failed_login_delay = 0
-
 [domain/${AD_DOMAIN,,}]
-realmd_tags = manages-system joined-with-adcli
 ad_domain = ${AD_DOMAIN,,}
-ad_server = ${AD_DOMAIN,,}
-id_provider = ad
-auth_provider = ad
-chpass_provider = ad
-access_provider = ad
-#ad_access_filter = (&(objectClass=inetOrgPerson)(employeeNumber=*))
-
-# Para descoberta de DNS
 krb5_realm = ${AD_DOMAIN^^}
-
-dyndns_update = False
-# Para habilitar DNS dinâmico:
-# dyndns_server = ${AD_DOMAIN,,}
-# dyndns_update = True
-# dyndns_update_ptr = True
-#dyndns_refresh_interval = 43200
-#dyndns_ttl = 3600
-
-# Para ter logins offline
+realmd_tags = manages-system joined-with-samba
 cache_credentials = True
+id_provider = ad
 krb5_store_password_if_offline = True
-
-# uid para login e criacao de pasta home
-enumerate = True
+default_shell = /bin/bash
+ldap_id_mapping = False
 use_fully_qualified_names = False
 fallback_homedir = /home/%u
-default_shell = /bin/bash
-
-# para usar uid and gid do active directory
-ldap_id_mapping = False
-
-# necessario para uso correto das propriedades do active directory
-ldap_schema = ad
-ldap_user_object_class = InetOrgPerson
-ldap_user_name = uid
-ldap_user_gid_number = gidNumber
-ldap_user_uid_number = uidNumber
-ldap_user_gecos = displayName
-" >/etc/sssd/conf.d/01-cmc.conf
+access_provider = ad
+#ad_access_filter = (memberOf=cn=admins,ou=Testou,dc=example,dc=com)
+# ad_access_filter = (embployeeNumber=*)
+# " >/etc/sssd/conf.d/01-cmc.conf
 
 # Altera permissão do arquivo sssd.conf
 chmod 600 /etc/sssd/conf.d/01-cmc.conf
 
 # Restarta serviços REALMD e SSSD
-systemctl restart realmd sssd
-
-# Garante que todas as contas de domínio tenham permissão para autenticação no computador
-#realm permit --all
+systemctl restart sssd
 
 # Testes com o AD
 #
