c120ac437d160ab8db1d3fc0af27c639e59710ab
Bruno Oliveira <bruno.oliveira@cmc.pr.gov.br>
2024-04-05 13:15:42 -0300

    feat(browser): atualizações automáticas edge

--- scripts/180-unattended-upgrades.sh
+++ scripts/180-unattended-upgrades.sh
@@ -21,12 +21,20 @@ UNATTENDEDCONF="/etc/apt/apt.conf.d/50unattended-upgrades"
 sed -i '/^\/\/Unattended-Upgrade::MinimalSteps/c\Unattended-Upgrade::MinimalSteps "true";' "$UNATTENDEDCONF"
 
 if apt-cache policy | grep -qE "release.+Google.+amd64"; then
-    GOOGLEPPA=$(apt-cache policy | grep -E "release.+Google.+amd64" | cut -d ',' -f 2 | cut -d '=' -f 2)
+    GOOGLEORIGIN=$(apt-cache policy | grep -E "release.+Google.+amd64" | cut -d ',' -f 2 | cut -d '=' -f 2)
 
-    if [[ -n "$GOOGLEPPA" ]]; then
-        sed -i '/Unattended-Upgrade::Allowed-Origins {/a\\t\"'"${GOOGLEPPA}"':stable\";' "$UNATTENDEDCONF"
+    if [[ -n "$GOOGLEORIGIN" ]]; then
+        sed -i '/Unattended-Upgrade::Allowed-Origins {/a\\t\"'"${GOOGLEORIGIN}"':stable\";' "$UNATTENDEDCONF"
     fi
 else
     echo "Source list do Chrome não encontrado, abortando."
     exit 1
+fi
+
+if apt-cache policy | grep -qE "release.+edge.+stable"; then
+    EDGEORIGIN=$(apt-cache policy | grep -E "release.+edge.+stable" | cut -d ',' -f 1 | cut -d '=' -f 2)
+
+    if [[ -n "$EDGEORIGIN" ]]; then
+        sed -i '/Unattended-Upgrade::Allowed-Origins {/a\\t\"'"${EDGEORIGIN}"':stable\";' "$UNATTENDEDCONF"
+    fi
 fi
