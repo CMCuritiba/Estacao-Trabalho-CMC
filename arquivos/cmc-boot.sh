7298b472d10291ba769f8efb34e30abd2e8dcb07
ewerton-dif <ewerton@cmc.pr.gov.br>
2024-07-17 10:14:10 -0300

    fix: arruma hostname errado da ET

--- arquivos/cmc-boot.sh
+++ arquivos/cmc-boot.sh
@@ -4,6 +4,7 @@ CMCDOMAIN=""
 INTERFACE="$(nmcli device status | grep -E "conectado|connected" | head -n1 | awk '{print $1}')"
 IPATUAL=$(ip addr list "$INTERFACE" | grep -m1 -w "inet" | awk '{print $2}' | cut -d '/' -f 1)
 
+# o computador tem rede?
 if [ -n "$IPATUAL" ] && [ -n "$CMCDOMAIN" ]; then
     # Obtem nome do IP consultando o DNS
     FQDN="$(nslookup "$IPATUAL" "$CMCDOMAIN" | head -n1 | awk '{print $4}')"
@@ -11,10 +12,17 @@ if [ -n "$IPATUAL" ] && [ -n "$CMCDOMAIN" ]; then
         # obtem o shortname
         DNSNAME="$(cut -d '.' -f 1 <<<"$FQDN")"
 
+        # o hostname atual eh o mesmo do DNS?
         if [ -n "$DNSNAME" ] && ! grep -q "$DNSNAME" <<<"$(hostname)"; then
-            # echo "$DNSNAME"
+
+            # Retira o computador com hostname antigo do domínio
+            echo "$AD_JOIN_PASS" | realm leave -U "$AD_JOIN_USER" "$CMCDOMAIN"
+
             # Atualiza hostname
             hostnamectl set-hostname "$DNSNAME"
+
+            # Inclui o computador com hostname novo no domínio
+            echo "$AD_JOIN_PASS" | realm join  -U "$AD_JOIN_USER" "$CMCDOMAIN"
         fi
     fi
 fi
